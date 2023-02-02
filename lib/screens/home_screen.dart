2d9bae01e927703326ac8d9893cdd09aa2f1bed0
Lans12345 <olanalans12345@gmail.com>
2023-02-02 13:04:21 +0800

    initial setup

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:mediscript/utils/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: primary,
+    );
   }
 }
---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+var primary = const Color(0xff3D8FA1);
+
+var secondary = const Color(0xff53B6CC);
+
+var light = const Color(0xffABE5F1);
