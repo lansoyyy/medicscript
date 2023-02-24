e1da43e3425315badee625b67d178b86684b09f6
Lans12345 <olanalans12345@gmail.com>
2023-02-24 10:07:55 +0800

    added db init

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:localstore/localstore.dart';
 import 'package:mediscript/utils/colors.dart';
 import 'package:mediscript/widgets/text_widget.dart';
 import 'package:http/http.dart' as http;
 import 'package:url_launcher/url_launcher.dart';
 
-class ResultScreen extends StatelessWidget {
+class ResultScreen extends StatefulWidget {
+  List<String> meds = [];
+
+  ResultScreen({required this.meds});
+
+  @override
+  State<ResultScreen> createState() => _ResultScreenState();
+}
+
+class _ResultScreenState extends State<ResultScreen> {
   Future<bool> linkExists(String url) async {
     final thisUrl = url;
 
@@ -19,9 +29,8 @@ class ResultScreen extends StatelessWidget {
     }
   }
 
-  List<String> meds = [];
+  final db = Localstore.instance;
 
-  ResultScreen({required this.meds});
   @override
   Widget build(BuildContext context) {
     final box = GetStorage();
@@ -41,12 +50,12 @@ class ResultScreen extends StatelessWidget {
         ],
       ),
       body: GridView.builder(
-          itemCount: meds.length,
+          itemCount: widget.meds.length,
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2),
           itemBuilder: ((context, index) {
             return FutureBuilder<bool>(
-                future: linkExists(meds[index].toLowerCase()),
+                future: linkExists(widget.meds[index].toLowerCase()),
                 builder: (context, snapshot) {
                   if (snapshot.connectionState == ConnectionState.waiting) {
                     return const SizedBox(); // Show empty space while waiting for the future
@@ -55,17 +64,26 @@ class ResultScreen extends StatelessWidget {
                   } else {
                     bool exists = snapshot.data!;
 
+                    if (exists == true) {
+                      final id = db.collection('History').doc().id;
+                      db.collection('Categ').doc(id).set({
+                        'name': widget.meds[index],
+                        'link':
+                            'https://www.drugs.com/${widget.meds[index].toLowerCase().trim()}.html'
+                      });
+                    }
+
                     return exists == true
                         ? Padding(
                             padding: const EdgeInsets.all(5.0),
                             child: GestureDetector(
                               onTap: () async {
                                 if (await canLaunch(
-                                    'https://www.drugs.com/${meds[index].toLowerCase().trim()}.html')) {
+                                    'https://www.drugs.com/${widget.meds[index].toLowerCase().trim()}.html')) {
                                   await launch(
-                                      'https://www.drugs.com/${meds[index].toLowerCase().trim()}.html');
+                                      'https://www.drugs.com/${widget.meds[index].toLowerCase().trim()}.html');
                                 } else {
-                                  throw 'Could not launch https://www.drugs.com/${meds[index].toLowerCase().trim()}.html';
+                                  throw 'Could not launch https://www.drugs.com/${widget.meds[index].toLowerCase().trim()}.html';
                                 }
                               },
                               child: Card(
@@ -89,7 +107,7 @@ class ResultScreen extends StatelessWidget {
                                             color: Colors.black54),
                                         child: ListTile(
                                           title: TextBold(
-                                              text: meds[index],
+                                              text: widget.meds[index],
                                               fontSize: 15,
                                               color: Colors.white),
                                         ),
